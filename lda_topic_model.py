e9e8fce4035729d6dc16c74434ec62f961f2a709
aviadazriel <47175833+aviadazriel@users.noreply.github.com>
2020-09-02 03:47:28 +0300

    Update lda_topic_model.py

--- lda_topic_model.py
+++ lda_topic_model.py
@@ -1,8 +1,71 @@
 from utils import *
 from preprocess import *
+import multiprocessing as mp
 
 from gensim.models.callbacks import PerplexityMetric
 
+def create_lda_turicreate(sframe, path_target):
+  # Create BOW:
+  docs = tc.text_analytics.count_words(random_tweets_sf['clean_text'])
+  # Train a topic model:
+  tc.config.set_runtime_config('TURI_DEFAULT_NUM_PYLAMBDA_WORKERS', mp.cpu_count()-2)
+  num_topics = 130
+  num_iterations = 100
+  topic_model = tc.topic_model.create(docs, num_topics=num_topics, num_iterations=num_iterations)
+  
+  # Save model:
+  topic_model.save(path_target)
+  return topic_model
+
+def create_lda_gensim(sframe, path_target):
+  
+  # Create bigrams:
+  min_count=5
+  threshold=100
+  bigram = gensim.models.Phrases(sf['tokens'], min_count=min_count, threshold=threshold)
+  bigram_mod = gensim.models.phrases.Phraser(bigram)
+  unigram_and_bigram = [bigram_mod[tweet] for tweet in sf['tokens']]
+  # Create Dictionary:
+  id2word = corpora.Dictionary(unigram_and_bigram)
+
+  # Term Document Frequency:
+  corpus = [id2word.doc2bow(tweet) for tweet in unigram_and_bigram]
+
+  #Build LDA model
+  perplexity_logger = PerplexityMetric(corpus=corpus, logger='shell')
+  coherence_logger  = gensim.models.callbacks.CoherenceMetric(corpus=corpus, coherence="u_mass", logger='shell')
+
+  # Config:
+  num_topics = 4
+  random_state = 11
+  update_every = 1
+  chunksize = 5000
+  passes=10
+  alpha='auto'
+  per_word_topics=True
+
+  lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                             id2word=id2word,
+                                             num_topics=num_topics, 
+                                             random_state=random_state,
+                                             update_every=update_every,
+                                             chunksize=chunksize,
+                                             passes=passes,
+                                             alpha=alpha, callbacks=[coherence_logger],
+                                             per_word_topics=per_word_topics)
+
+  lda_model.save(path_target)
+
+  #Evaluate
+  coherence_model_lda = CoherenceModel(model=lda_model, texts=unigram_and_bigram, dictionary=id2word, coherence='c_v')
+  coherence_lda = coherence_model_lda.get_coherence()
+  print('Coherence Score: ', coherence_lda)
+
+  # Show topics:
+  for index, topic in lda_model.show_topics(formatted=False, num_words= 10):
+    print('Topic: {} \nWords: {}'.format(index+1, [w[0] for w in topic]))
+
+  
 #prepair the data 
 sfraim_path = 'all_tweets.sframe'
 sf = tc.load_sframe(sfraim_path)
@@ -24,49 +87,14 @@ sf['tokens'] = sf['clean_text'].apply(lambda x: x.split())
 sf = sf[['tokens']]
 sf.materialize()
 
-# Create bigrams:
-min_count=5
-threshold=100
-bigram = gensim.models.Phrases(sf['tokens'], min_count=min_count, threshold=threshold)
-bigram_mod = gensim.models.phrases.Phraser(bigram)
-unigram_and_bigram = [bigram_mod[tweet] for tweet in sf['tokens']]
-# Create Dictionary:
-id2word = corpora.Dictionary(unigram_and_bigram)
-
-# Term Document Frequency:
-corpus = [id2word.doc2bow(tweet) for tweet in unigram_and_bigram]
-
-#Build LDA model
-perplexity_logger = PerplexityMetric(corpus=corpus, logger='shell')
-coherence_logger  = gensim.models.callbacks.CoherenceMetric(corpus=corpus, coherence="u_mass", logger='shell')
-
-# Config:
-num_topics = 4
-random_state = 11
-update_every = 1
-chunksize = 5000
-passes=10
-alpha='auto'
-per_word_topics=True
-
-lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
-                                           id2word=id2word,
-                                           num_topics=num_topics, 
-                                           random_state=random_state,
-                                           update_every=update_every,
-                                           chunksize=chunksize,
-                                           passes=passes,
-                                           alpha=alpha, callbacks=[coherence_logger],
-                                           per_word_topics=per_word_topics)
-
 model_path = './drive/My Drive/big_data/models/lda/lda.model'
-lda_model.save(model_path)
+lda_gensim = False
+if lda_gensim:
+  print('create LDA Gensim Model')
+  lda_model = create_lda_gensim(sf,lda_gensim)
 
-#Evaluate
-coherence_model_lda = CoherenceModel(model=lda_model, texts=unigram_and_bigram, dictionary=id2word, coherence='c_v')
-coherence_lda = coherence_model_lda.get_coherence()
-print('Coherence Score: ', coherence_lda)
+else:
+  print('create LDA turicreate Model')
+  lda_model = create_lda_turicreate(sf,lda_gensim)
 
-# Show topics:
-for index, topic in lda_model.show_topics(formatted=False, num_words= 10):
-  print('Topic: {} \nWords: {}'.format(index+1, [w[0] for w in topic]))
+
